1998777cf4d47ebdc6639d94730d6ada1918e9d1
TadewosBell <bell6@umbc.edu>
2018-04-28 12:43:52 -0400

    intial commit

---
+++ Surgeries.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get("http://www.rightdiagnosis.com/surgery/abdominal-liposuction.htm")
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+procedure = soup.find(id='wd_content')
+
+title = soup.findAll('h1')
+
+print(title)
+
+subtitles = soup.find_all('h2')
+
+print(subtitles)
+
+Desc = []
+#for i in subtitles:
+#	Desc.append(soup.find(text = i.get_text()).findNext('p').contents)
+
+#for x in Desc:
+	#print x
